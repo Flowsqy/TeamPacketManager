20eca18e88ccaa243d2393c069aabac47cda7da3
Flowsqy <Flo504.Dev@gmail.com>
2021-03-27 13:52:41 +0100

    Implement TeamPacketManager

--- src/main/java/fr/flowsqy/teampacketmanager/TeamPacketManager.java
+++ src/main/java/fr/flowsqy/teampacketmanager/TeamPacketManager.java
@@ -1,13 +1,15 @@
 package fr.flowsqy.teampacketmanager;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.Plugin;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class TeamPacketManager implements Listener {
 
@@ -15,11 +17,65 @@ public class TeamPacketManager implements Listener {
 
     public TeamPacketManager(Plugin plugin) {
         data = new HashMap<>();
+        Bukkit.getPluginManager().registerEvents(this, plugin);
+    }
+
+    public TeamData applyTeamData(Player player, TeamData teamData){
+        if(player == null)
+            return null;
+        final String playerName = player.getName();
+        final TeamData previousData = data.get(playerName);
+        if(teamData == null)
+            return previousData;
+        if(previousData == null){
+            data.put(playerName, teamData);
+            TeamPacketSender.sendTeamData(
+                    Bukkit.getOnlinePlayers(),
+                    teamData,
+                    new ArrayList<>(Collections.singletonList(playerName)),
+                    TeamPacketSender.Method.CREATE
+            );
+            return null;
+        }
+        final TeamData conflictData = previousData.merge(teamData);
+        final boolean changeName = !conflictData.getId().equals(TeamData.DEFAULT_TEAM_ID);
+        if(changeName){
+            removeTeam(conflictData.getId());
+        }
+        TeamPacketSender.sendTeamData(
+                Bukkit.getOnlinePlayers(),
+                previousData,
+                new ArrayList<>(Collections.singletonList(playerName)),
+                changeName ? TeamPacketSender.Method.CREATE : TeamPacketSender.Method.UPDATE_INFO
+        );
+        return conflictData;
+    }
+
+    public TeamData removeTeamData(String player){
+        final TeamData teamData = data.remove(player);
+        if(teamData != null && !teamData.getId().equals(TeamData.DEFAULT_TEAM_ID)){
+            removeTeam(teamData.getId());
+        }
+        return teamData;
+    }
+
+    private void removeTeam(String id){
+        TeamPacketSender.sendTeamData(
+                Bukkit.getOnlinePlayers(),
+                new TeamData(id),
+                new ArrayList<>(),
+                TeamPacketSender.Method.REMOVE
+        );
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
     private void onQuit(PlayerQuitEvent event){
-        data.remove(event.getPlayer().getName());
+        removeTeamData(event.getPlayer().getName());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    private void onJoin(PlayerJoinEvent event){
+
     }
 
 }
