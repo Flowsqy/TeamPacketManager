257ada1cac5abda5b4f6e6224374596d6b92a880
Flowsqy <Flo504.Dev@gmail.com>
2021-03-27 11:38:56 +0100

    Add plugin structure

---
+++ src/main/java/fr/flowsqy/teampacketmanager/TeamPacketManagerPlugin.java
@@ -0,0 +1,13 @@
+package fr.flowsqy.teampacketmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TeamPacketManagerPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+
+    }
+}
