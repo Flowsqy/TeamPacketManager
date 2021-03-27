44831b168df8f365df285beaa00061e15b9f6d87
Flowsqy <Flo504.Dev@gmail.com>
2021-03-27 13:54:14 +0100

    Init TeamPacketManager in TeamPacketManagerPlugin

--- src/main/java/fr/flowsqy/teampacketmanager/TeamPacketManagerPlugin.java
+++ src/main/java/fr/flowsqy/teampacketmanager/TeamPacketManagerPlugin.java
@@ -4,10 +4,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class TeamPacketManagerPlugin extends JavaPlugin {
 
+    private TeamPacketManager teamPacketManager;
+
     @Override
     public void onEnable() {
-
-
-
+        teamPacketManager = new TeamPacketManager(this);
     }
+
+    public TeamPacketManager getTeamPacketManager() {
+        return teamPacketManager;
+    }
+
 }
