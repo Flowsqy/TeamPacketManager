eaa5557b4bae1abb3ffe6218593cc77f702b1986
Flowsqy <Flo504.Dev@gmail.com>
2021-04-10 16:54:14 +0200

    Add multiple TeamData support to permute id

--- src/main/java/fr/flowsqy/teampacketmanager/TeamPacketManager.java
+++ src/main/java/fr/flowsqy/teampacketmanager/TeamPacketManager.java
@@ -1,5 +1,6 @@
 package fr.flowsqy.teampacketmanager;
 
+import fr.flowsqy.teampacketmanager.commons.ApplicableTeamData;
 import fr.flowsqy.teampacketmanager.commons.TeamData;
 import fr.flowsqy.teampacketmanager.exception.TeamIdException;
 import fr.flowsqy.teampacketmanager.io.Messages;
@@ -37,6 +38,7 @@ public class TeamPacketManager implements Listener {
 
     /**
      * Lock state
+     *
      * @return true if packets are locked, false otherwise
      */
     public boolean isLocked() {
@@ -45,6 +47,7 @@ public class TeamPacketManager implements Listener {
 
     /**
      * Lock or unlock packets
+     *
      * @param locked true to lock packets, false to unlock
      */
     public void setLocked(boolean locked) {
@@ -86,6 +89,7 @@ public class TeamPacketManager implements Listener {
 
     /**
      * Get the current TeamData
+     *
      * @param player The player who is linked to the team data
      * @return The team data
      */
@@ -94,15 +98,192 @@ public class TeamPacketManager implements Listener {
     }
 
     /**
+     * Apply multiple TeamData as the same time. This allow you to change permute id
+     *
+     * @param applicableList The list of all Team that will be sent
+     */
+    public synchronized void applyTeamData(List<ApplicableTeamData> applicableList) {
+        final Set<String> players = new HashSet<>(applicableList.size());
+        final Set<String> ids = new HashSet<>(applicableList.size());
+        // Find same values and register all player names and ids
+        for (ApplicableTeamData applicable : applicableList) {
+            if (applicable == null)
+                continue;
+            if (!applicable.getTeamData().canSend())
+                continue;
+            if (!players.add(applicable.getPlayer().getName()))
+                throw new RuntimeException(
+                        "Attempting to change the team data for player '"
+                                + applicable.getPlayer().getName()
+                                + "' two times in same instruction. Can not select the good id and pass id check");
+            if (!ids.add(applicable.getTeamData().getId()))
+                throw new TeamIdException("Attempting to register same id for different players : " + applicable.getTeamData().getId());
+        }
+        final List<Object> packets = new ArrayList<>(ids.size());
+        final Set<String> removed = new HashSet<>();
+        final Set<String> updated = new HashSet<>();
+        for (ApplicableTeamData applicable : applicableList) {
+            final TeamData newData = applicable.getTeamData();
+            final String id = newData.getId();
+            final String previousPlayerName = idPlayer.get(id);
+            final boolean update = previousPlayerName != null;
+            // Id is already set but will not be updated -> Same id for two teams (registered and to register)
+            if (update && !players.contains(previousPlayerName)) {
+                throw new TeamIdException("The id '" + id + "' is already taken by the player '" + previousPlayerName + "'");
+            }
+
+            final String playerName = applicable.getPlayer().getName();
+            final TeamData currentData = data.get(playerName);
+
+            // Player has not previous team data -> register it server side
+            if (currentData == null) {
+                if (!locked) {
+                    try {
+                        // The team already exist client side -> update it
+                        if (update) {
+                            updateTeam(packets, previousPlayerName, playerName, newData, id);
+                            // If the team will be removed, remove it from remove list, add it as updated otherwise
+                            if (!removed.remove(id)) {
+                                updated.add(id);
+                            }
+                        }
+                        // The team does not exist client side -> create it
+                        else {
+                            packets.add(TeamPacketSender.getPacket(
+                                    newData,
+                                    Collections.singletonList(playerName),
+                                    TeamPacketSender.Method.CREATE
+                                    )
+                            );
+                        }
+                    } catch (ReflectiveOperationException exception) {
+                        throw new RuntimeException(exception);
+                    }
+                }
+                data.put(playerName, newData);
+                idPlayer.put(id, playerName);
+                continue;
+            }
+
+            // Player has a previous team data
+
+            // The new data is same as the old one
+            if (currentData.equals(newData)) {
+                continue;
+            }
+
+            // Update the data
+            final TeamData conflictData = currentData.merge(newData);
+
+            if (!locked) {
+                try {
+                    // Exist client side -> Update it client side
+                    if (update) {
+                        updateTeam(packets, previousPlayerName, playerName, currentData, id);
+                        // If the team will be removed, remove it from remove list, add it as updated otherwise
+                        if (!removed.remove(id)) {
+                            updated.add(id);
+                        }
+                    }
+                    // Or Create a new team client side but the player has an old team data (both side)
+                    else {
+                        // Remove from updated, add to remove list if has not been updated yet
+                        if (!updated.remove(conflictData.getId())) {
+                            // Add to remove list old id that is not updated yet
+                            removed.add(conflictData.getId());
+                        }
+                        // Add the new team client side
+                        packets.add(TeamPacketSender.getPacket(
+                                currentData,
+                                Collections.singletonList(playerName),
+                                TeamPacketSender.Method.CREATE
+                                )
+                        );
+                    }
+                } catch (ReflectiveOperationException exception) {
+                    throw new RuntimeException(exception);
+                }
+            }
+
+            // Register the new id
+            idPlayer.put(id, playerName);
+        }
+        // Remove all unused teams
+        for (String removeId : removed) {
+            if (!locked) {
+                try {
+                    packets.add(TeamPacketSender.getPacket(
+                            new TeamData(removeId),
+                            Collections.emptyList(),
+                            TeamPacketSender.Method.REMOVE
+                            )
+                    );
+                } catch (ReflectiveOperationException exception) {
+                    throw new RuntimeException(exception);
+                }
+            }
+            idPlayer.remove(removeId);
+        }
+
+        // Send packets
+        taskManager.subscribeAll(packets);
+    }
+
+    private void updateTeam(
+            List<Object> packets,
+            String previousPlayerName,
+            String playerName,
+            TeamData teamData,
+            String id)
+            throws ReflectiveOperationException {
+        // Remove old player
+        packets.add(TeamPacketSender.getPacket(
+                id,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                Collections.singletonList(previousPlayerName),
+                TeamPacketSender.Method.REMOVE_PLAYERS.getMethod(),
+                0
+                )
+        );
+        // Update info
+        packets.add(TeamPacketSender.getPacket(
+                teamData,
+                Collections.emptyList(),
+                TeamPacketSender.Method.UPDATE_INFO
+                )
+        );
+        // Add new player
+        packets.add(TeamPacketSender.getPacket(
+                id,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                Collections.singletonList(playerName),
+                TeamPacketSender.Method.ADD_PLAYERS.getMethod(),
+                0
+                )
+        );
+    }
+
+    /**
      * Apply a custom team data to a player
-     * @param player The targeted player
+     *
+     * @param player   The targeted player
      * @param teamData The data to send
      * @return The old TeamData, can be null if player was not registered
-     * @throws NullPointerException if the player is null
+     * @throws NullPointerException     if the player is null
      * @throws IllegalArgumentException if the team data can not be sent
-     * @throws TeamIdException if the id is already taken by another player
+     * @throws TeamIdException          if the id is already taken by another player
      */
-    public TeamData applyTeamData(Player player, TeamData teamData) {
+    public synchronized TeamData applyTeamData(Player player, TeamData teamData) {
         Objects.requireNonNull(player);
         final String playerName = player.getName();
         final TeamData previousData = data.get(playerName);
@@ -184,6 +365,7 @@ public class TeamPacketManager implements Listener {
 
     /**
      * Remove the TeamData of a player
+     *
      * @param player The targeted player
      * @return The data who is removed
      */
@@ -209,6 +391,7 @@ public class TeamPacketManager implements Listener {
 
     /**
      * Send a remove packet
+     *
      * @param id the id of the team to remove
      */
     private void removeTeam(String id) {
@@ -227,6 +410,7 @@ public class TeamPacketManager implements Listener {
 
     /**
      * Quit event handle method to remove the data of player who leave the server
+     *
      * @param event The PlayerQuitEvent
      */
     @EventHandler(priority = EventPriority.MONITOR)
@@ -237,6 +421,7 @@ public class TeamPacketManager implements Listener {
 
     /**
      * Join event handle method to send data of players who are already connected
+     *
      * @param event The PlayerJoinEvent
      */
     @EventHandler(priority = EventPriority.MONITOR)
